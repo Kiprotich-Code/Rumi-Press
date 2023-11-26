95c9564c963aea1ea7545cee8c82cefa813ecfd6
Lawi Kiprotich <fleeklawi@gmail.com>
2023-11-26 19:19:47 +0300

    model author

--- source/main/models.py
+++ source/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Authors():
+    name = models.CharField(max_length=100)
+    country_of_origin = models.CharField(max_length=25, blank=True)
+    date_born = models.DateTimeField()
+    date_died = models.DateTimeField(blank=True)
+    interests = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
