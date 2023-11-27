42c40f64afda292383bd9361893caffdce9ada4a
Lawi Kiprotich <fleeklawi@gmail.com>
2023-11-27 10:50:32 +0300

    Worked on the Author's page

--- source/main/models.py
+++ source/main/models.py
@@ -6,6 +6,7 @@ class Authors(models.Model):
     country_of_origin = models.CharField(max_length=25, blank=True)
     date_born = models.DateField(blank=True)
     interests = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
     
     def __str__(self):
         return self.name
