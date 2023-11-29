b3c9376ce8e11e193a89824cf998dabfcf6f3021
Lawi Kiprotich <fleeklawi@gmail.com>
2023-11-29 20:52:31 +0300

    Fix - migration issue

--- source/main/admin.py
+++ source/main/admin.py
@@ -3,8 +3,5 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Authors, Books, Category, Publisher, Expense
 
 # Register your models here.
-admin.site.register(Authors)
-admin.site.register(Books)
-admin.site.register(Category)
 admin.site.register(Publisher)
-admin.site.register(Expense)
+admin.site.register(Category)
